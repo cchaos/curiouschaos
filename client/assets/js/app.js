651b05831d177a0676151c20df1f5c1c178fec48
Caroline Horn <chorn@42six.com>
2017-03-02 11:32:46 -0500

    Fixed HTML5 mode refresh on Apache server
    run `bower-install`

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -4,6 +4,7 @@
   angular.module('application', [
     'ui.router',
     'ngAnimate',
+    'ngRoute',
 
     //foundation
     'foundation',
@@ -17,10 +18,36 @@
     .run(run)
   ;
 
-  config.$inject = ['$urlRouterProvider', '$locationProvider'];
+  config.$inject = ['$urlRouterProvider', '$locationProvider', '$routeProvider'];
 
-  function config($urlProvider, $locationProvider) {
+  function config($urlProvider, $locationProvider, $routeProvider) {
     $urlProvider.otherwise('/');
+
+    $routeProvider
+      .when('/spheric', {
+        templateUrl: 'projects/spheric.html',
+      })
+      .when('/cinario', {
+        templateUrl: 'projects/cinario.html',
+      })
+      .when('/the-road', {
+        templateUrl: 'projects/road.html',
+      })
+      .when('/misadventures', {
+        templateUrl: 'projects/misadventures.html',
+      })
+      .when('/reinventing-michael-banks', {
+        templateUrl: 'projects/tmb.html',
+      })
+      .when('/will', {
+        templateUrl: 'projects/will.html',
+      })
+      .when('/weddings', {
+        templateUrl: 'projects/weddings.html',
+      })
+      .when('/about', {
+        templateUrl: 'about.html',
+      });
 
     $locationProvider.html5Mode({
       enabled: true,
